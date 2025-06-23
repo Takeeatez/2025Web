6f2235e958ac8778a5893cef0abebf152543d340
None <137224269+Takeeatez@users.noreply.github.com>
2025-06-23 14:52:08 +0900

    변수 미니퀘스트

---
+++ variable.js
@@ -0,0 +1,15 @@
+// 변수 미니퀘스트
+// Q1
+
+let me = '최이지';
+let age = 22;
+let height = 170.0;
+
+console.log(me);
+console.log(age);
+console.log(height);
+
+// Q2
+console.log('\n');
+console.log('Q2');
+console.log(me + ',' + age + ',' + height);
---
+++ variable_quest.js
@@ -0,0 +1,9 @@
+let write = '1. 작성';
+let show = '2. 조회';
+let edit = '3. 수정';
+let del = '4. 삭제';
+let add = '5. 추가기능';
+let quit = '6. 종료';
+
+console.log(write + ' ' + show + ' ' + edit + ' ' + del + ' ' + add + ' ' + quit);
+
